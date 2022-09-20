8a111993b9bf45a9c2e4fffb008449815eb6a0be
InExSu <InExSu@bk.ru>
2022-09-20 23:41:16 +0300

    handLocal

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
 // service-account-01@popov-php-bitrix24-zg-20220603.iam.gserviceaccount.com
 
 // Connect the Google Sheets API client
+use Google\Service\Sheets\ClearValuesResponse;
 use Google\Service\Sheets\UpdateValuesResponse;
 use Google\Service\Sheets\ValueRange;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once 'Google.php';
 
-$File_Credentials  = __DIR__ . '/popov-php-bitrix24-zg-20220603-a1df757bd724.json';
+$File_Credentials          = __DIR__ . '/popov-php-bitrix24-zg-20220603-a1df757bd724.json';
 $googleSheet_Range_Address = "2021 год"; // весь диапазон листа
 $googleSheet_Spread_ID     = '18B7ifEIqg0GHyET5AX-RM0ZVv1Sm9phJWJNRbNaH0SM'; // Контакты с выставки
 
@@ -36,14 +37,29 @@ function bitrix24_Leads_from_Array($object,
 
 /** диапазон гугтаблицы в массив */
 function googleSheet_Range_2_Array(string $googleSheet_Range_Address,
-                           string $googleSheet_spreadSheets_ID,
-                           string $file_Credentials): ValueRange
+                                   string $googleSheet_spreadSheets_ID,
+                                   string $file_Credentials): ValueRange
 {
     $google        = new Google($file_Credentials,
                                 $googleSheet_spreadSheets_ID);
     $googleService = $google->service_Sheets();
     return $googleService->spreadsheets_values->get($googleSheet_spreadSheets_ID,
                                                     $googleSheet_Range_Address);
+}
+
+/** диапазон гугл таблицы очистить */
+function googleSheet_Range_Clear(string $range_Address,
+                                 string $spreadSheet_ID,
+                                 string $file_Credentials): ClearValuesResponse
+{
+    $google        = new Google($file_Credentials,
+                                $spreadSheet_ID);
+    $googleService = $google->service_Sheets();
+    $requestBody   = new Google_Service_Sheets_ClearValuesRequest();
+
+    return $googleService->spreadsheets_values->clear($spreadSheet_ID,
+                                                      $range_Address,
+                                                      $requestBody);
 }
 
 /** массив в гуглтаблицу */
@@ -65,8 +81,31 @@ function array_2_Google_Sheet(array $array,
                                                        $arr_Options);
 }
 
-
-function bitrix24($webHook)
+/** массив Php в массив для диапазона гугл таблицы */
+function array_Php_2_Array_Sheet($arr_Php): array
 {
+// Todo ошибка: Invalid JSON payload received. Unknown name \"1\" at 'data.values': Cannot find field.\
 
+
+    $max = count($arr_Php);
+
+    $arr_Sheet = [];
+
+    for ($row = 1; $row < $max; $row++) {
+
+        foreach ($arr_Php[$row] as $key => $elem) {
+            if (is_array($elem)) {
+                $stop = true;
+                if (isset($elem['downloadUrl'])) {
+                    $arr_Sheet[$row][] = $elem['downloadUrl'];
+                } else {
+                    // todo где проскакивает вложенный массив
+                }
+
+            } else {
+                $arr_Sheet[$row][] = ($arr_Php[$row][$key] == null) ? '' : $arr_Php[$row][$key];
+            }
+        }
+    }
+    return $arr_Sheet;
 }
